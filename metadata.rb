50a4700b2812b6789cd303c4d4fafdc49e2a56dd
Yevgeniy A. Viktorov <wik@osmonitoring.com>
2010-09-01 08:12:30 +0300

    bump version number

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Yevgeniy Viktorov"
 maintainer_email "wik@rentasite.com.ua"
 license          "Apache 2.0"
 description      "build eAccelerator from sources"
-version          "0.1.0"
+version          "0.1.1"
 recipe           "eaccelerator", "build eAccelerator from sources"
 
 %w{ debian ubuntu redhat centos fedora suse }.each do |os|
